53475f0b30fe39bc51a5b8e776c3481479252d1a
Romain Bonnefille <rbonnefille@zendesk.com>
2025-02-21 11:49:03 +0100

    Add command-line arguments to filter_users.py for dynamic input and output file handling

--- filter_users.py
+++ filter_users.py
@@ -1,7 +1,16 @@
 import csv
+import sys
+import argparse
 
-input_file = 'appUserExport.csv'
-output_file = 'filteredUsers.csv'
+parser = argparse.ArgumentParser(description='Filter users based on number of conversations.')
+parser.add_argument('--row', type=str, required=True, help='numberOfConversations')
+parser.add_argument('--input', type=str, required=True, help='appUsers.csv')
+parser.add_argument('--output', type=str, required=True, help='filteredUsers.csv')
+args = parser.parse_args()
+
+row_to_filter = args.row
+input_file = args.input
+output_file = args.output
 
 with open(input_file, mode='r', newline='') as infile, open(output_file, mode='w', newline='') as outfile:
     reader = csv.DictReader(infile)
@@ -10,5 +19,5 @@ with open(input_file, mode='r', newline='') as infile, open(output_file, mode='w
     
     writer.writeheader()
     for row in reader:
-        if int(row['numberOfConversations']) > 1: # can be changed to another header
+        if int(row[row_to_filter]) > 1: # can be changed to another header
             writer.writerow(row)
